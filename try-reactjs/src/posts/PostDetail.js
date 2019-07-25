028af99b03736253ebe357dd87ad3e2d61006f38
alexHooper <alexmohack@gmail.com>
2019-07-25 08:33:38 +0530

    use class extending Component to use props

--- try-reactjs/src/posts/PostDetail.js
+++ try-reactjs/src/posts/PostDetail.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function PostDetail () {
-	const {post} = this.props
+class PostDetail extends Component {
+	render () {
+		const {post} = this.props
 
-	return (
-		<div >
-			<h2>{post.title}</h2>
-			<p>{post.content}</p>
-		</div>
-	)
+		return (
+			<div >
+				<h2>{post.title}</h2>
+				<p>{post.content}</p>
+			</div>
+		)
+	}
 }
 
 export default PostDetail
