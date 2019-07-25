b01bb0c4eeb7902ed2425acbe067572bdcb7bed8
alexHooper <alexmohack@gmail.com>
2019-07-25 08:52:47 +0530

    use onClick event with props

--- try-reactjs/src/posts/PostDetail.js
+++ try-reactjs/src/posts/PostDetail.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
 class PostDetail extends Component {
+	constructor (props) {
+		super(props)
+		this.titleWasClicked = this.titleWasClicked.bind(this)
+	}
+
+	titleWasClicked (event) {
+		console.log(this.props)
+	}
+
 	render () {
 		const {post} = this.props
 
 		return (
 			<div >
-				<h2>{post.title}</h2>
+				<h2 onClick={this.titleWasClicked}>{post.title}</h2>
 				<p>{post.content}</p>
 			</div>
 		)
