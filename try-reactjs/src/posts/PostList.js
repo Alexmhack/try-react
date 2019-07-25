96a5ab6599097d1ae8da4064ae384e3174ee9179
alexHooper <alexmohack@gmail.com>
2019-07-25 09:09:09 +0530

    use class and define custom property

--- try-reactjs/src/posts/PostList.js
+++ try-reactjs/src/posts/PostList.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PostData from '../data/posts.json'
 import PostDetail from './PostDetail'
 
-function PostList () {
-  return (
-	<div >
-		<h1>Posts are apppearing below.</h1>
-		{PostData.map((item, index) => {
-			return <PostDetail post={item} key={`post-list-key ${index}`} />
-		})}
-	</div>
-  )
+class PostList extends Component {
+	constructor (props) {
+		super(props)
+		this.handleDataCallback = this.handleDataCallback.bind(this)
+	}
+
+	handleDataCallback (msg) {
+		console.log(msg)
+	}
+
+	render () {
+	  return (
+		<div >
+			<h1>Posts are apppearing below.</h1>
+			{PostData.map((item, index) => {
+				return <PostDetail
+					post={item}
+					key={`post-list-key ${index}`}
+					dataCallback={this.handleDataCallback}	// arbitrary property handled by function
+				/>
+			})}
+		</div>
+	  )
+	}
 }
 
 export default PostList
