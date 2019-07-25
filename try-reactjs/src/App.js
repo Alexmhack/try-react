022eaa9c2387c7d088e5be2f9eeb6cc1c6991523
alexHooper <alexmohack@gmail.com>
2019-07-25 14:43:25 +0530

    display youtube video

--- try-reactjs/src/App.js
+++ try-reactjs/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import PostList from './posts/PostList'
+// import PostList from './posts/PostList'
+import ReactYoutubeExample from './thirdparty/ReactYoutubeExample'
 
 function App () {
   return (
     <div className='App'>
       <header className='App-header'>
-        <PostList />
+        <ReactYoutubeExample videoId='_nBlN9yp9R8' />
       </header>
     </div>
   )
 }
-
+ 
 export default App
