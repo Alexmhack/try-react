2ad41503f97d06930d1e39d64257cca566e93545
alexHooper <alexmohack@gmail.com>
2019-07-25 16:02:31 +0530

    display react markdown'

--- try-reactjs/src/App.js
+++ try-reactjs/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import PostList from './posts/PostList'
 // import ReactYoutubeExample from './thirdparty/ReactYoutubeExample'
+import ReactMarkdownExample from './thirdparty/ReactMarkdownExample'
 
 // <ReactYoutubeExample videoId='_nBlN9yp9R8' />
 // <PostList />
@@ -9,6 +10,7 @@ function App () {
   return (
     <div className='App'>
       <header className='App-header'>
+      	<ReactMarkdownExample />
       </header>
     </div>
   )
