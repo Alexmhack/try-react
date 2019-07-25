a46fc8ae5966067f58a60856c868a130b56952ac
alexHooper <alexmohack@gmail.com>
2019-07-25 08:31:42 +0530

    use props to display data

--- try-reactjs/src/posts/PostDetail.js
+++ try-reactjs/src/posts/PostDetail.js
@@ -1,1 +1,14 @@
-import 'react' from React
+import React from 'react'
+
+function PostDetail () {
+	const {post} = this.props
+
+	return (
+		<div >
+			<h2>{post.title}</h2>
+			<p>{post.content}</p>
+		</div>
+	)
+}
+
+export default PostDetail
