40391d9a56f7e3738a0ba99552f55a92e6529dff
alexHooper <alexmohack@gmail.com>
2019-07-25 15:08:39 +0530

    comment out tags

--- try-reactjs/src/App.js
+++ try-reactjs/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 // import PostList from './posts/PostList'
-import ReactYoutubeExample from './thirdparty/ReactYoutubeExample'
+// import ReactYoutubeExample from './thirdparty/ReactYoutubeExample'
+
+// <ReactYoutubeExample videoId='_nBlN9yp9R8' />
+// <PostList />
 
 function App () {
   return (
     <div className='App'>
       <header className='App-header'>
-        <ReactYoutubeExample videoId='_nBlN9yp9R8' />
       </header>
     </div>
   )
