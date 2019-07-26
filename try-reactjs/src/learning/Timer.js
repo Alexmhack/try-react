7f4719cfbc83260ae202a5fdbfeb52591a9d2484
alexHooper <alexmohack@gmail.com>
2019-07-26 12:04:50 +0530

    create timer component

---
+++ try-reactjs/src/learning/Timer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Timer extends Component {
+	constructor (props) {
+		super(props)
+	}
+
+	render () {
+		return (
+			<div>Hello world</div>
+		)
+	}
+}
+
+export default Timer
