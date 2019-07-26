af8d66cba24e8e491f2080ad8fa74835d87441ad
alexHooper <alexmohack@gmail.com>
2019-07-26 12:05:05 +0530

    use timer component

--- try-reactjs/src/App.js
+++ try-reactjs/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import PostSorting from './posts/PostSorting'
+// import PostSorting from './posts/PostSorting'
 // import PostList from './posts/PostList'
 // import ReactYoutubeExample from './thirdparty/ReactYoutubeExample'
 // import ReactMarkdownExample from './thirdparty/ReactMarkdownExample'
+
+import Timer from './learning/Timer'
 
 // <ReactYoutubeExample videoId='_nBlN9yp9R8' />
 // <PostList />
@@ -12,7 +14,7 @@ function App () {
   return (
     <div className='App'>
       <header className='App-header'>
-      	<PostSorting />
+      	<Timer />
       </header>
     </div>
   )
