c4d8a0cbc17260ee5588b12838f5c715df247094
alexHooper <alexmohack@gmail.com>
2019-07-27 09:25:48 +0530

    use current state for incrementing count

--- try-reactjs/src/learning/Timer.js
+++ try-reactjs/src/learning/Timer.js
@@ -3,11 +3,24 @@ import React, { Component } from 'react'
 class Timer extends Component {
 	constructor (props) {
 		super(props)
+
+		this.state = {
+			count: 0
+		}
+	}
+
+	componentDidMount () {
+		const myInterval = setInterval(() => {
+			this.setState(prevState => ({
+				count: this.state.count + 1
+			}))
+		}, 1000)
 	}
 
 	render () {
+		const {count} = this.state
 		return (
-			<div>Hello world</div>
+			<div>Current Count: {count}</div>
 		)
 	}
 }
