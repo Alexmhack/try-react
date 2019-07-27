60a22b98a9620520dd5dddfe687ec4b48e84949d
alexHooper <alexmohack@gmail.com>
2019-07-27 08:25:23 +0530

    use react-video-recoreder

---
+++ try-reactjs/src/thirdparty/VideoRecorderExample.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import VideoRecorder from 'react-video-recorder'
+
+class VideoRecorderExample extends Component {
+	render () {
+		return (
+			<div>
+				<VideoRecorder />
+			</div>
+		)
+	}
+}
+
+export default VideoRecorderExample
