6e78094f4eb8bdbb0418642b27ec43d0259e436a
alexHooper <alexmohack@gmail.com>
2019-07-27 21:00:01 +0530

    import input reusable component

--- try-reactjs/src/learning/FormsAndInput.js
+++ try-reactjs/src/learning/FormsAndInput.js
@@ -1,23 +1,5 @@
 import React, { Component } from 'react'
-
-function InputElement (props) {
-	function handleChange (event) {
-		if (props.onChange) props.onChange(event)
-	}
-
-	return (
-		<div>
-			<input
-				ref={props.inputRef}
-				name={props.name}
-				type={props.type}
-				value={props.value}
-				placeholder={props.placeholder}
-				onChange={handleChange}
-			/>
-		</div>
-	)
-}
+import InputElement from './InputElement'
 
 class FormsAndInput extends Component {
 	constructor (props) {
