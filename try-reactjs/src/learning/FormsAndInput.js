106b7b758287f2092f2edbf08a4e702016a1b1e7
alexHooper <alexmohack@gmail.com>
2019-07-27 21:38:18 +0530

    use InputBlock and set state

--- try-reactjs/src/learning/FormsAndInput.js
+++ try-reactjs/src/learning/FormsAndInput.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react'
 import InputElement from './InputElement'
+import InputBlock from './InputBlock'
 
 class FormsAndInput extends Component {
 	constructor (props) {
 		super(props)
 		this.state = {
-			fullName: null
+			fullName: null,
 		}
 		this.inputFullNameRef = React.createRef()
 	}
 
 	handleSubmit = (event) => {
 		event.preventDefault()
-		// const data = this.state
-		// console.log(data)
-		console.log(this.inputFullNameRef.current.value)
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+		const data = this.state
+		console.log(data)
+		// console.log(this.inputFullNameRef.current.value)
 	}
 
 	handleInputChange = (event) => {
@@ -53,8 +57,11 @@ class FormsAndInput extends Component {
 				<p>Full Name: {fullName}</p>
 				<form action='.' method='POST' onSubmit={this.handleSubmit}>
 					<input ref={this.inputFullNameRef} type='text' name='fullName' onChange={this.handleInputChange} placeholder='Your Name' /><br />
-					<textarea required={true} ref={node => this.inputContentRef = node} placeholder='Type your content'></textarea><br />
-					<InputElement name='email' type='email' placeholder='Your email' inputRef={node => this.inputElementRef = node} /><br />
+					<textarea name='content' required={true} ref={node => this.inputContentRef = node} onChange={this.handleInputChange} placeholder='Type your content'></textarea><br />
+					<InputElement onChange={this.handleInputChange} name='email' type='email' placeholder='Your email' inputRef={node => this.inputElementRef = node} /><br />
+
+					<InputBlock onChange={this.handleInputChange} name='number' type='number' placeholder='Your phone number' inputRef={node => this.inputElementRef = node} /><br />
+
 					<button type='submit'>Save</button><br />
 					<button onClick={this.handleFocusClick} type='submit'>Focus</button><br />
 					<button onClick={this.handleClearClick} type='submit'>Clear</button><br />
