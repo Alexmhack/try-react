ec9a00c429b5a964d8954c470ce53c82d86b7e51
alexHooper <alexmohack@gmail.com>
2019-07-27 09:29:34 +0530

    clear interval on unmount

--- try-reactjs/src/learning/Timer.js
+++ try-reactjs/src/learning/Timer.js
@@ -10,17 +10,24 @@ class Timer extends Component {
 	}
 
 	componentDidMount () {
-		const myInterval = setInterval(() => {
+		this.myInterval = setInterval(() => {
 			this.setState(prevState => ({
 				count: prevState.count + 1
 			}))
 		}, 1000)
 	}
 
+	componentWillUnmount() {
+		// clear the interval in case of memory leaks
+		clearInterval(this.myInterval)
+	}
+
 	render () {
 		const {count} = this.state
 		return (
-			<div>Current Count: {count}</div>
+			<div>
+				<h1>Current Count: {count}</h1>
+			</div>
 		)
 	}
 }
