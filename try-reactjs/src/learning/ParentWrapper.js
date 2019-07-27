278a92103c02e0867726cb055c9334a0a305e887
alexHooper <alexmohack@gmail.com>
2019-07-27 11:44:45 +0530

    create func using alertbox

--- try-reactjs/src/learning/ParentWrapper.js
+++ try-reactjs/src/learning/ParentWrapper.js
@@ -13,4 +13,9 @@ function AlertBox (props) {
 	return <div className={'alert alert-' + props.alertType}>{props.children}</div>
 }
 
+function WelcomeDialog (props) {
+	return <AlertBox alertType='success'><h1>This is to welcome the users</h1></AlertBox>
+}
+
+export {WelcomeDialog}
 export default AlertBox
