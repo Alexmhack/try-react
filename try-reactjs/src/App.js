97776fa81cf1ecb95fdf5c0da1c77c9e448e5b01
alexHooper <alexmohack@gmail.com>
2019-07-27 09:25:13 +0530

    use timer

--- try-reactjs/src/App.js
+++ try-reactjs/src/App.js
@@ -3,9 +3,9 @@ import React from 'react'
 // import PostList from './posts/PostList'
 // import ReactYoutubeExample from './thirdparty/ReactYoutubeExample'
 // import ReactMarkdownExample from './thirdparty/ReactMarkdownExample'
-// import Timer from './learning/Timer'
+import Timer from './learning/Timer'
 // import VideoCaptureExample from './thirdparty/VideoCaptureExample'
-import VideoRecorderExample from './thirdparty/VideoRecorderExample'
+// import VideoRecorderExample from './thirdparty/VideoRecorderExample'
 
 // <ReactYoutubeExample videoId='_nBlN9yp9R8' />
 // <PostList />
@@ -15,7 +15,7 @@ function App () {
   return (
     <div className='App'>
       <header className='App-header'>
-      	<VideoRecorderExample />
+      	<Timer />
       </header>
     </div>
   )
