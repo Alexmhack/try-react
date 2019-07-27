34024ce7d3e42722f23aee249e75eb120178e16e
alexHooper <alexmohack@gmail.com>
2019-07-27 08:38:12 +0530

    only working in chrome

--- try-reactjs/src/thirdparty/VideoRecorderExample.js
+++ try-reactjs/src/thirdparty/VideoRecorderExample.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import VideoRecorder from 'react-video-recorder'
 
+// only working in chrome, not on firefox
 class VideoRecorderExample extends Component {
 	render () {
 		return (
