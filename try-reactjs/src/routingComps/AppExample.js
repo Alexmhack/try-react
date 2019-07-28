22bea1f1c051be1de4aa41e8f5819f9f0573ac48
alexHooper <alexmohack@gmail.com>
2019-07-28 11:01:55 +0530

    correct import errors

--- try-reactjs/src/routingComps/AppExample.js
+++ try-reactjs/src/routingComps/AppExample.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
 import StaticRouterExample from './StaticRouterExample'
+import DynamicRouteComp from './DynamicRoutingExample'
+import NotFound from './NotFound'
 
 class AppRoutingExample extends Component {
 	render () {
 		const loggedIn = true
-		const supportsHistor = 'pushstate' in window.history
+		const supportsHistory = 'pushstate' in window.history
+
+		// firefox works without => forceRefresh={!supportsHistory}
 
 		return (
 	    <div className='App' >
@@ -16,8 +20,8 @@ class AppRoutingExample extends Component {
 	        </nav>
 	        <BrowserRouter forceRefresh={!supportsHistory}>
 	          <Switch>
-	            <Route exact path='/' component={StaticRouteComp} />
-	            <Route exact path='/about' component={StaticRouteComp} />
+	            <Route exact path='/' component={StaticRouterExample} />
+	            <Route exact path='/about' component={StaticRouterExample} />
 	            <Route path='/posts/:slug' component={DynamicRouteComp} />
 	            <Route component={NotFound} />
 
@@ -25,7 +29,7 @@ class AppRoutingExample extends Component {
 	              loggedIn === true ? (
 	                <Redirect to='/posts/hello-there/' />
 	              ) : (
-	                <StaticRouteComp />
+	                <StaticRouterExample />
 	              )
 	            )} />
 
