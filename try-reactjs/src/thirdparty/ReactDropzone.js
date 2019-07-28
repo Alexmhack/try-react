d4297780d1c01a7ea6237b14f69cefc408213647
alexHooper <alexmohack@gmail.com>
2019-07-28 08:05:14 +0530

    use condition check for file sizes

--- try-reactjs/src/thirdparty/ReactDropzone.js
+++ try-reactjs/src/thirdparty/ReactDropzone.js
@@ -2,13 +2,40 @@ import React, { Component } from 'react'
 import Dropzone from 'react-dropzone'
 
 class ReactDropzone extends Component {
+	constructor (props) {
+		super(props)
+		
+		this.state = {
+			maxFileSize: 10
+		}
+	}
+
 	handleOnDrop = (files, rejectedFiles) => {
-		console.log(files)
-		console.log(rejectedFiles)
+		if (files && files.length > 0) {
+			const currentFile = files[0]
+			const currentFileSize = currentFile.size
+			const currentFileType = currentFile.type
+			const {maxFileSize} = this.state
+
+			if (currentFileSize > maxFileSize) {
+				alert('File is too big')
+			}
+		}
+
+		if (rejectedFiles && rejectedFiles.length > 0) {
+			const currentFile = rejectedFiles[0]
+			const currentFileSize = currentFile.size
+			const currentFileType = currentFile.type
+			const {maxFileSize} = this.state
+
+			if (currentFileSize > maxFileSize) {
+				alert('File is too big')
+			}
+		}
 	}
 
 	render () {
-		const maxFileSize = 1000000000000000000000000
+		const {maxFileSize} = this.state
 
 		return (
 			<div>
